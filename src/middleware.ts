7866a6be83374ca6917aa64379e2f9f7e4b5cbf1
Peter Yoo <trustintofu@gmail.com>
2023-03-08 00:32:14 -0800

    feat(middleware): Adds redirect if user has no cookie session.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,14 @@
+import { NextRequest, NextResponse } from "next/server";
 export { default } from "next-auth/middleware";
+
+export function middleware(req: NextRequest) {
+  const cookie = req.cookies.get("next-auth.session-token")?.value;
+  if (cookie === undefined) {
+    const url = req.nextUrl.clone();
+    url.pathname = "/";
+    return NextResponse.redirect(url, req.url);
+  }
+}
 
 export const config = {
   matcher: ["/dashboard"],
