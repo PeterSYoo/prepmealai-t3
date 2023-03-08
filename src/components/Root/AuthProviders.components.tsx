84a3e5d98013108d072e11ced49f1e1d27ade47e
Peter Yoo <trustintofu@gmail.com>
2023-03-07 23:20:10 -0800

    refactor: Moves components to new folder.

--- src/components/Root/AuthProviders.components.tsx
+++ src/components/Root/AuthProviders.components.tsx
@@ -1,20 +1,12 @@
 import { signIn, signOut, useSession } from "next-auth/react";
-import { api } from "~/utils/api";
-// import { env } from "~/env.mjs";
 
 export const AuthProviders: React.FC = () => {
   const { data: sessionData } = useSession();
 
-  const { data: secretMessage } = api.example.getSecretMessage.useQuery(
-    undefined, // no input
-    { enabled: sessionData?.user !== undefined }
-  );
-
   return (
     <div className="flex w-full max-w-[400px] flex-col items-center justify-center gap-4">
       <p className="text-center text-2xl">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-        {secretMessage && <span> - {secretMessage}</span>}
+        {sessionData && <span>Logged in as {sessionData.user?.email}</span>}
       </p>
       <button
         className="w-full border border-black bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-[#6a6967] hover:text-white"
