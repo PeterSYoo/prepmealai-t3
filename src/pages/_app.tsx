de95d2673f59c153caf8cdfc4abb6e1d3512f9a7
Peter Yoo <trustintofu@gmail.com>
2023-03-08 23:27:07 -0800

    feat(_app): Adds div tag with h-screen.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,7 +12,9 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <div className="h-screen">
+        <Component {...pageProps} />
+      </div>
     </SessionProvider>
   );
 };
