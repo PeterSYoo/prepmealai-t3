4ef8a56dcfa9a9033604b82ed677bf89a1d38409
Peter Yoo <trustintofu@gmail.com>
2023-03-08 16:46:50 -0800

    refactor(useHandleLogin): Adds eslint ignores.

--- src/hooks/useHandleLogin.ts
+++ src/hooks/useHandleLogin.ts
@@ -1,23 +1,22 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { useMutation } from "@tanstack/react-query";
+import type { NextRouter } from "next/router";
 
 type Data = {
   email: string;
   password: string;
 };
 
-type SignIn = {
-  redirect: boolean;
-  email: string;
-  password: string;
-  callbackUrl: string;
-};
-
 const useHandleLogin = (
-  signIn: (arg0: string, arg1: SignIn) => any,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  signIn: any,
   setIsModal: (arg0: boolean) => void,
-  router: any
+  router: NextRouter
 ) => {
-  const { mutateAsync, isLoading } = useMutation(async (data: any) => {
+  const { mutateAsync, isLoading } = useMutation(async (data: Data) => {
     try {
       const status = await signIn("credentials", {
         redirect: false,
@@ -29,7 +28,9 @@ const useHandleLogin = (
       if (status.error === "No user found with that email!") {
         setIsModal(true);
       } else if (status.ok) {
-        router.push(status.url);
+        if (status.ok && status.url) {
+          await router.push(status.url);
+        }
       }
     } catch (error) {
       return error;
