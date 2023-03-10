c63e6a0bce22ec4fe2b5a14f4ab73f39bbfe97e5
Peter Yoo <trustintofu@gmail.com>
2023-03-09 23:37:36 -0800

    feat(DashboardPage): Creates function to handle open ai prompt, and make mutation request on our procedural and utilizes useEffect to store the returned data in a state. Passes down props to components.

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,24 +1,63 @@
 import { signOut, useSession } from "next-auth/react";
+import { Fragment, useEffect, useState } from "react";
+import { Demo } from "~/components/dashboard/Demo.components";
 import { GenerateRecipeForm } from "~/components/dashboard/GenerateRecipeForm.components";
 import { RecipeCard } from "~/components/dashboard/RecipeCard.components";
 import { api } from "~/utils/api";
 
 const DashboardPage = () => {
+  const [recipe, setRecipe] = useState<any>([]);
   const mutation = api.openai.postOpenai.useMutation();
   // session
   const { data: sessionData, status } = useSession();
 
-  const handleOpenai = () => {
-    const content = "What is 3+3?";
+  const handleGenerateRecipe = (
+    calories: string,
+    protein: string,
+    meat: string
+  ) => {
+    const content = `
+    Create a random recipe with ${calories} calories and ${protein}g of ${meat} protein.
+    Do not include any explanations, only provide a  RFC8259 compliant JSON response  following this format without deviation.
+    [
+      {
+        "name": "name of recipe",
+        "dishType": "type of dish",
+        "description": "description of recipe",
+        "ingredients": ["ingredient 1", "ingredient 2", "ingredient 3", "etc"],
+        "calories": "number of calories",
+        "protein": "number of protein by gram",
+        "fat": "number of fat by gram",
+        "carb": "number of carb by gram",
+        "prepTime": "preperation time",
+        "cookingTime": "cooking time",
+        "instructions": [
+          {
+            "step 1": "step 1 instructions",
+            "step 2": "step 2 instructions",
+            "step 3": "step 3 instructions",
+            "step 4": "etc."
+          }
+        ]
+      }
+    ]    
+    The JSON response:
+    `;
 
     mutation.mutate({ content });
   };
 
-  console.log({ mutation });
+  useEffect(() => {
+    if (mutation.data?.data.choices[0]) {
+      setRecipe(JSON.parse(mutation.data?.data.choices[0].message.content));
+    }
+  }, [mutation.data]);
 
   if (status === "loading") {
     return <>Fetching User Data..</>;
   }
+
+  console.log({ recipe });
 
   return (
     <>
@@ -31,22 +70,13 @@ const DashboardPage = () => {
       >
         Sign Out
       </button>
-      <button
-        onClick={handleOpenai}
-        className="w-full border border-black bg-white/10 px-10 py-3 font-semibold transition hover:bg-[#6a6967] hover:text-white"
-      >
-        OpenAi
-      </button>
-      <p className="text-center">
-        {mutation.data?.data.choices[0].message.content}
-      </p>
       <div className="grid grid-cols-[1.25fr_1fr] gap-[75px] bg-gradient-to-b from-[#abd1d9] to-[#ffffff] p-14">
         {/* Column 1 */}
         <div className="w-full">
           <h1 className="">Generate Recipes</h1>
           <div className="flex w-full flex-col border border-black px-5 py-10">
             {/* Generate Recipe Form */}
-            <GenerateRecipeForm />
+            <GenerateRecipeForm handleGenerateRecipe={handleGenerateRecipe} />
             {/*  */}
             {/* Results */}
             <div className="flex flex-col gap-3">
@@ -55,7 +85,7 @@ const DashboardPage = () => {
                 <div className="mx-auto w-full border-b border-black"></div>
               </div>
               {/* Recipe Card */}
-              <RecipeCard />
+              <RecipeCard recipe={recipe} />
               {/*  */}
             </div>
             {/*  */}
@@ -68,6 +98,9 @@ const DashboardPage = () => {
           <div className="w-full border border-black">Test</div>
         </div>
         {/*  */}
+      </div>
+      <div>
+        <Demo />
       </div>
     </>
   );
