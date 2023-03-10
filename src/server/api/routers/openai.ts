fa120eef4f23090da85a4cdd5c9e5573b9be651c
Peter Yoo <trustintofu@gmail.com>
2023-03-09 20:29:34 -0800

    feat(openaiRouter): Creates router and postOpenai procedural that makes an API call.

---
+++ src/server/api/routers/openai.ts
@@ -0,0 +1,43 @@
+import { z } from "zod";
+import { createTRPCRouter, protectedProcedure } from "../trpc";
+import { env } from "~/env.mjs";
+
+console.log(env.OPENAI_API_KEY);
+
+export const openaiRouter = createTRPCRouter({
+  postOpenai: protectedProcedure
+    .input(z.object({ content: z.string() }))
+    .mutation(async ({ input }) => {
+      try {
+        const response = await fetch(
+          "https://api.openai.com/v1/chat/completions",
+          {
+            method: "POST",
+            headers: {
+              Authorization: `Bearer ${env.OPENAI_API_KEY}`,
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              model: "gpt-3.5-turbo",
+              messages: [
+                {
+                  role: "user",
+                  content: input.content,
+                },
+              ],
+            }),
+          }
+        );
+
+        const data = await response.json();
+
+        return {
+          success: true,
+          message: "OpenAI generated 3 recipes.",
+          data,
+        };
+      } catch (error) {
+        console.log(error);
+      }
+    }),
+});
