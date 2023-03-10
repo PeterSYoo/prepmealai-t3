a238def2809edcb7a6d9e7ae6609245a1481e09d
Peter Yoo <trustintofu@gmail.com>
2023-03-09 23:34:58 -0800

    feat(openaiRouter): Creates postDemo procedural for demo purposes.

--- src/server/api/routers/openai.ts
+++ src/server/api/routers/openai.ts
@@ -33,7 +33,42 @@ export const openaiRouter = createTRPCRouter({
 
         return {
           success: true,
-          message: "OpenAI generated 3 recipes.",
+          message: "OpenAI generated a recipe in JSON format.",
+          data,
+        };
+      } catch (error) {
+        console.log(error);
+      }
+    }),
+  postDemo: protectedProcedure
+    .input(z.object({ content: z.string() }))
+    .mutation(async ({ input }) => {
+      try {
+        const response = await fetch(
+          "https://api.openai.com/v1/chat/completions",
+          {
+            method: "POST",
+            headers: {
+              Authorization: `Bearer ${env.OPENAI_API_KEY}`,
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              model: "gpt-3.5-turbo",
+              messages: [
+                {
+                  role: "user",
+                  content: input.content,
+                },
+              ],
+            }),
+          }
+        );
+
+        const data = await response.json();
+
+        return {
+          success: true,
+          message: "OpenAI generated demo object.",
           data,
         };
       } catch (error) {
