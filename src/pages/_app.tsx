5ec59f50865149434718d7ef86d9458a4b0a9f3a
Peter Yoo <trustintofu@gmail.com>
2023-03-12 23:45:31 -0700

    feat(_app): Imports google fonts using next font.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { PT_Sans_Narrow } from "next/font/google";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+const ptSansNarrow = PT_Sans_Narrow({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-ptSansNarrow",
+});
 
 import { api } from "~/utils/api";
 
@@ -12,7 +25,9 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <div className="h-screen">
+      <div
+        className={`${inter.variable} ${ptSansNarrow.variable} h-screen font-inter`}
+      >
         <Component {...pageProps} />
       </div>
     </SessionProvider>
