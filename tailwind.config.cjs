94f1e895021068d63796c4d08133626643fde157
Peter Yoo <trustintofu@gmail.com>
2023-03-12 23:44:51 -0700

    feat(tailwind.config.cjs): Adds pt sans narrow and inter fonts.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,10 @@ const config = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      ptSansNarrow: ["var(--font-ptSansNarrow)"],
+      inter: ["var(--font-inter)"],
+    },
   },
   plugins: [],
 };
