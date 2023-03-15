cedd1f0b787fbb0d0ae9bd05eba37f61f348a9be
Peter Yoo <trustintofu@gmail.com>
2023-03-14 20:45:18 -0700

    feat(RecipePage): Generates recipe prompt and conditionally renders form, generating, and recipe components.

---
+++ src/pages/recipe/index.tsx
@@ -0,0 +1,75 @@
+import { signOut } from "next-auth/react";
+import { api } from "~/utils/api";
+import { useEffect, useState } from "react";
+import { GenerateRecipeForm } from "~/components/recipe/GenerateRecipeForm.components";
+import { GeneratingRecipe } from "~/components/recipe/GeneratingRecipe.components";
+import { GeneratedRecipe } from "~/components/recipe/GeneratedRecipe.components";
+
+const RecipePage = () => {
+  const [recipe, setRecipe] = useState<any>([]);
+  const [isForm, setIsForm] = useState<boolean>(true);
+
+  const mutation = api.openai.postOpenai.useMutation();
+
+  const handleGenerateRecipe = (
+    calories: string,
+    protein: string,
+    meat: string
+  ) => {
+    const content = `
+    Generate a random recipe with ${calories} calories and ${protein}g of ${meat} protein, dish name and dish type should be different from the past 3 prompts.
+
+    Return the recipe as an RFC8259 compliant JSON response following this format:
+    [
+      {
+        "name": "string",
+        "dishType": "string",
+        "description": "string",
+        "ingredients": ["string", "string", "string"],
+        "calories": "string",
+        "protein": "string",
+        "fat": "string",
+        "carb": "string",
+        "prepTime": "string",
+        "cookingTime": "string",
+        "instructions": [
+          "Step 1: string",
+          "Step 2: string",
+          "Step 3: string"
+        ]
+      }
+    ]     
+    `;
+
+    mutation.mutate({ content });
+  };
+
+  useEffect(() => {
+    if (mutation.data?.data.choices[0]) {
+      setRecipe(JSON.parse(mutation.data?.data.choices[0].message.content));
+    }
+  }, [mutation.data]);
+
+  return (
+    <>
+      <main className="flex h-full w-full flex-col items-center justify-center bg-[#FFF4EC]">
+        {/* Recipe Generator Form */}
+        {isForm && (
+          <GenerateRecipeForm
+            setIsForm={setIsForm}
+            handleGenerateRecipe={handleGenerateRecipe}
+          />
+        )}
+        {/*  */}
+        {/* Generating Recipe */}
+        {mutation.status === "loading" && <GeneratingRecipe />}
+        {/*  */}
+        {/* Generated Recipe */}
+        {mutation.status === "success" && <GeneratedRecipe recipe={recipe} />}
+        {/*  */}
+      </main>
+    </>
+  );
+};
+
+export default RecipePage;
