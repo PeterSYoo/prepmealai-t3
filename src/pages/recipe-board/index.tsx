30b0cd7d8f4f201cfcb3b6dd519d1874e434bead
Peter Yoo <trustintofu@gmail.com>
2023-03-14 20:45:32 -0700

    feat(RecipeBoardPage): Gets all recipes and displays them.

---
+++ src/pages/recipe-board/index.tsx
@@ -0,0 +1,25 @@
+import { Fragment } from "react";
+import { SavedRecipesCard } from "~/components/dashboard/SavedRecipesCard.components";
+import { api } from "~/utils/api";
+
+const RecipeBoardPage = () => {
+  const allRecipes = api.recipe.getAllRecipes.useQuery();
+
+  console.log({ allRecipes: allRecipes?.data?.recipes });
+
+  return (
+    <>
+      <div className="">
+        <div className="flex flex-col gap-8 pt-28">
+          {allRecipes?.data?.recipes?.map((recipe) => (
+            <Fragment key={recipe?.id}>
+              <SavedRecipesCard recipe={recipe} refetch={allRecipes?.refetch} />
+            </Fragment>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default RecipeBoardPage;
