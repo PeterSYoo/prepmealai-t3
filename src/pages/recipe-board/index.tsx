6a395cbd0039f50fd3845148c0940ad1cb0823a7
Peter Yoo <trustintofu@gmail.com>
2023-03-19 02:48:16 -0700

    feat(RecipeBoardPage): Implements drag and drop functionality of recipe cards, and when you drag a card over the delete area it will make a procedural call to delete the resource from the database.

--- src/pages/recipe-board/index.tsx
+++ src/pages/recipe-board/index.tsx
@@ -1,31 +1,67 @@
-import { DndContext, closestCenter } from "@dnd-kit/core";
+import {
+  DndContext,
+  closestCenter,
+  useDroppable,
+  DragOverlay,
+  useSensors,
+  useSensor,
+  PointerSensor,
+  KeyboardSensor,
+} from "@dnd-kit/core";
 import {
   arrayMove,
   SortableContext,
   verticalListSortingStrategy,
   horizontalListSortingStrategy,
   rectSortingStrategy,
+  sortableKeyboardCoordinates,
 } from "@dnd-kit/sortable";
-import { Fragment, useEffect, useState } from "react";
-import { SavedRecipesCard } from "~/components/dashboard/SavedRecipesCard.components";
+import { Fragment, useCallback, useEffect, useState } from "react";
+import { DroppableDeleteArea } from "~/components/recipe-board/DroppableDeleteArea.components";
+import { RecipeDragOverlay } from "~/components/recipe-board/RecipeDragOverlay.components";
+import { SavedRecipesCard } from "~/components/recipe-board/SavedRecipesCard.components";
 import { api } from "~/utils/api";
 
 const RecipeBoardPage = () => {
+  const [activeId, setActiveId] = useState<any>(null);
   const [recipes, setRecipes] = useState<any>([]);
+  const [shouldDelete, setShouldDelete] = useState<boolean>(false);
   const allRecipes = api.recipe.getAllRecipes.useQuery();
+  const deleteRecipe = api.recipe.deleteRecipe.useMutation();
 
-  const handleDragEnd = (event: any) => {
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  );
+
+  const handleDragStart = (event) => {
+    console.log("Drag start called");
+    const { active } = event;
+
+    setActiveId(active.id);
+  };
+
+  const handleDragEnd = async (event: any) => {
     console.log("Drag end called");
     const { active, over } = event;
     console.log("ACTIVE: " + active.id);
     console.log("OVER :" + over.id);
 
-    if (active.id !== over.id) {
+    console.log("over: ", over);
+    if (over && over.id === "delete-container") {
+      console.log("Recipe deleted:", active.id);
+      await deleteRecipe.mutateAsync({ id: active.id }); // Make the delete request
+      setRecipes((items: any) =>
+        items.filter((item: any) => item.id !== active.id)
+      ); // Remove the item from the state
+    } else if (active.id !== over.id) {
       setRecipes((items: any) => {
-        const activeIndex = items.findIndex((item) => item.id === active.id);
-        const overIndex = items.findIndex((item) => item.id === over.id);
-        console.log({ activeIndex });
-        console.log({ overIndex });
+        const activeIndex = items.findIndex(
+          (item: any) => item.id === active.id
+        );
+        const overIndex = items.findIndex((item: any) => item.id === over.id);
 
         const newItems = arrayMove(items, activeIndex, overIndex);
         return newItems;
@@ -41,22 +77,22 @@ const RecipeBoardPage = () => {
 
   return (
     <>
-      <DndContext collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
-        <main className="h-full bg-[#FFF9F5]">
-          <div className="mx-auto bg-[#FFF9F5] px-5 md:max-w-[1440px]">
-            <div className="mx-auto flex flex-wrap justify-start gap-[50px] bg-[#FFF9F5] px-[105px] pt-[220px] md:columns-3">
-              {/* Delete Recipe Container */}
-              <div className="fixed top-0 left-0 z-0 w-full rounded-b-[25px]">
-                <div className="flex h-[150px] items-center justify-center rounded-b-[25px] border-b border-dashed border-[#534941] bg-[#DBD7CC]">
-                  <h1 className="font-bold text-[#000000]">
-                    Drag here to delete a recipe.
-                  </h1>
-                </div>
-              </div>
-              {/*  */}
-              {/* Recipe Cards */}
-              <div className="flex flex-wrap justify-center gap-[50px]">
-                <SortableContext items={recipes} strategy={rectSortingStrategy}>
+      <DndContext
+        sensors={sensors}
+        collisionDetection={closestCenter}
+        onDragStart={handleDragStart}
+        onDragEnd={handleDragEnd}
+        id="0"
+      >
+        <SortableContext items={recipes} strategy={rectSortingStrategy}>
+          <main className="h-full bg-[#FFF9F5]">
+            <div className="mx-auto bg-[#FFF9F5] px-5 md:max-w-[1440px]">
+              <div className="mx-auto flex flex-wrap justify-start gap-[50px] bg-[#FFF9F5] px-[105px] pt-[220px] md:columns-3">
+                {/* Delete Recipe Container */}
+                <DroppableDeleteArea />
+                {/*  */}
+                {/* Recipe Cards */}
+                <div className="flex flex-wrap justify-center gap-[50px]">
                   {recipes?.map((recipe, i) => (
                     <SavedRecipesCard
                       key={recipe.id}
@@ -65,6 +101,7 @@ const RecipeBoardPage = () => {
                       i={i}
                     />
                   ))}
+
                   {/* Add empty placeholders to align last row to start */}
                   {recipes &&
                     [...Array(Math.max(0, 3 - (recipes.length % 3)))].map(
@@ -75,12 +112,20 @@ const RecipeBoardPage = () => {
                         ></div>
                       )
                     )}
-                </SortableContext>
+                </div>
+                {/*  */}
               </div>
-              {/*  */}
             </div>
-          </div>
-        </main>
+          </main>
+        </SortableContext>
+        <DragOverlay>
+          {activeId ? (
+            <RecipeDragOverlay
+              id={activeId}
+              recipe={recipes.find((recipe) => recipe.id === activeId)}
+            />
+          ) : null}
+        </DragOverlay>
       </DndContext>
     </>
   );
