3bbf0fc75064519ad30d2ed94da9b626012ff894
Peter Yoo <trustintofu@gmail.com>
2023-03-18 17:13:08 -0700

    feat(Error): Creates error component for error handling when open ai doesn't return a proper JSON object.

---
+++ src/components/recipe/Error.components.tsx
@@ -0,0 +1,29 @@
+export const Error = ({
+  setIsError,
+  setIsForm,
+}: {
+  setIsError: (arg0: boolean) => void;
+  setIsForm: (arg0: boolean) => void;
+}) => {
+  const handleFormReturn = () => {
+    setIsError(false);
+    setIsForm(true);
+  };
+
+  return (
+    <>
+      <section className="flex h-full w-full max-w-[700px] items-center justify-center">
+        <p className="pb-[200px] text-center text-xl">
+          The AI failed to generate the recipe, sorry for the inconvenience, you
+          can go back to the&nbsp;
+          <button
+            className="font-bold text-orange-400"
+            onClick={handleFormReturn}
+          >
+            recipe generator page and try again.
+          </button>
+        </p>
+      </section>
+    </>
+  );
+};
