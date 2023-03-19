5afa79eb6ac173eed515923da0e905c8e9318a47
Peter Yoo <trustintofu@gmail.com>
2023-03-18 17:14:20 -0700

    style(MyApp): Adds border and updates positioning and z index positions.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -21,9 +21,7 @@ import "~/styles/globals.css";
 import { useRouter } from "next/router";
 import { MenuDrawer } from "~/components/MenuDrawer.components";
 import { useState } from "react";
-import { FiChevronsDown, FiChevronsUp, FiMenu } from "react-icons/fi";
-import { ImMenu3 } from "react-icons/im";
-import { GiHamburger } from "react-icons/gi";
+import { FiChevronsUp } from "react-icons/fi";
 import { HiOutlineSwitchVertical } from "react-icons/hi";
 
 const MyApp: AppType<{ session: Session | null }> = ({
@@ -48,32 +46,32 @@ const MyApp: AppType<{ session: Session | null }> = ({
   return (
     <SessionProvider session={session}>
       <div
-        className={`${inter.variable} ${ptSansNarrow.variable} h-screen bg-[#FFF9F5] font-inter`}
+        className={`${inter.variable} ${ptSansNarrow.variable} min-h-screen bg-[#FFF9F5] font-inter`}
       >
         <div
-          className={`absolute top-0 left-0 flex h-screen w-full transform flex-col items-center justify-center transition-all duration-300 ${
+          className={`absolute top-0 left-0 z-20 flex w-full transform flex-col items-center justify-center transition-all duration-300 ${
             isMenuDrawerOpen
-              ? "h-[55px] translate-y-0"
-              : "h-8 -translate-y-full"
+              ? "h-[50px] translate-y-0"
+              : "h-10 -translate-y-full"
           }`}
         >
           {shouldRenderHeader && <MenuDrawer />}
         </div>
-        <div className="flex justify-center">
+        <div className="z-20 flex justify-center">
           <button
-            className={`absolute transition-all duration-300 ${
-              isMenuDrawerOpen ? "top-[80px]" : "top-6"
+            className={`absolute z-20 transition-all duration-300 ${
+              isMenuDrawerOpen ? "top-[65px]" : "top-6"
             }`}
             onClick={isMenuDrawerOpen ? closeMenuDrawer : openMenuDrawer}
           >
             {shouldRenderHeader && (
               <>
                 {isMenuDrawerOpen ? (
-                  <div className="rounded-full bg-[#FFF9F5] p-2">
+                  <div className="rounded-full border border-[#D9CCC3] bg-[#FFF9F5] p-2">
                     <FiChevronsUp className="text-xl hover:hover:text-[#4e565f]" />
                   </div>
                 ) : (
-                  <div className="rounded-full bg-[#FFF9F5] p-2">
+                  <div className="rounded-full border border-[#D9CCC3] bg-[#FFF9F5] p-2">
                     <HiOutlineSwitchVertical className="text-xl hover:hover:text-[#4e565f]" />
                   </div>
                 )}
