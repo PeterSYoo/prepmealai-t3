e51d0b022619dfdffd3f62027e6e77d5b089c57b
Peter Yoo <trustintofu@gmail.com>
2023-03-18 17:16:01 -0700

    feat(openAiRouter): Creates a schema to check our returned data if it's in the right format, if not return an error object.

--- src/server/api/routers/openai.ts
+++ src/server/api/routers/openai.ts
@@ -2,6 +2,24 @@ import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 import { env } from "~/env.mjs";
 
+// Define the schema for the expected JSON format
+const recipeSchema = z.array(
+  z.object({
+    name: z.string(),
+    dishType: z.string(),
+    proteinChoice: z.string(),
+    description: z.string(),
+    ingredients: z.array(z.string()),
+    calories: z.string(),
+    protein: z.string(),
+    fat: z.string(),
+    carb: z.string(),
+    prepTime: z.string(),
+    cookingTime: z.string(),
+    instructions: z.array(z.string()),
+  })
+);
+
 export const openaiRouter = createTRPCRouter({
   postOpenai: protectedProcedure
     .input(z.object({ content: z.string() }))
@@ -27,13 +45,26 @@ export const openaiRouter = createTRPCRouter({
           }
         );
 
-        const data = await response.json();
-
-        return {
-          success: true,
-          message: "OpenAI generated a recipe in JSON format.",
-          data,
-        };
+        try {
+          const data = await response.json();
+          console.log(data.choices[0]);
+          // Check if the data matches the expected format
+          const parsedData = await JSON.parse(data.choices[0].message.content);
+          recipeSchema.parse(parsedData);
+          return {
+            success: true,
+            message: "OpenAI generated a recipe in JSON format.",
+            data,
+          };
+        } catch (error) {
+          console.log(error);
+          return {
+            success: false,
+            message:
+              "OpenAI generated data that does not match the expected format.",
+            data: null,
+          };
+        }
       } catch (error) {
         console.log(error);
       }
