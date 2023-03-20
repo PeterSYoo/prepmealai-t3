6a2a6fd10387f8cf180ec6783fec5ddf6c516cd9
Peter Yoo <trustintofu@gmail.com>
2023-03-20 01:00:22 -0700

    refactor(SavedRecipes): Removes mapped recipes component.

--- src/components/dashboard/SavedRecipes.components.tsx
+++ src/components/dashboard/SavedRecipes.components.tsx
@@ -1,6 +1,5 @@
 import { Fragment } from "react";
 import { api } from "~/utils/api";
-import { SavedRecipesCard } from "./SavedRecipesCard.components";
 
 export const SavedRecipes = () => {
   const allRecipes = api.recipe.getAllRecipes.useQuery();
@@ -10,13 +9,7 @@ export const SavedRecipes = () => {
   return (
     <>
       <div className="">
-        <div className="flex flex-col gap-8">
-          {allRecipes?.data?.recipes?.map((recipe) => (
-            <Fragment key={recipe?.id}>
-              <SavedRecipesCard recipe={recipe} refetch={allRecipes?.refetch} />
-            </Fragment>
-          ))}
-        </div>
+        <div className="flex flex-col gap-8"></div>
       </div>
     </>
   );
