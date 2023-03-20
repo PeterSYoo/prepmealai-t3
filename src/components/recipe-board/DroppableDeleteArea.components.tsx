46465d564cb90bf6aa9ebde76860c9311ec76b1f
Peter Yoo <trustintofu@gmail.com>
2023-03-20 01:01:59 -0700

    style(DroppableDeleteArea): Updates margins and border color.

--- src/components/recipe-board/DroppableDeleteArea.components.tsx
+++ src/components/recipe-board/DroppableDeleteArea.components.tsx
@@ -13,12 +13,14 @@ export const DroppableDeleteArea = () => {
   return (
     <>
       <div
-        className="fixed top-0 left-0 z-0 w-full rounded-b-[25px]"
+        className="fixed top-0 left-0 z-10 w-full rounded-b-[25px]"
         ref={setNodeRef}
         style={style}
       >
-        <div className="flex h-[150px] items-center justify-center rounded-b-[25px] border-b border-dashed border-[#534941] bg-[#DBD7CC]">
-          <h1 className="font-bold text-[#000000]">Drag here to delete</h1>
+        <div className="flex h-[150px] items-center justify-center rounded-b-[25px] border-b border-dashed border-[#b7afaa] bg-[#DBD7CC]">
+          <h1 className="pt-[50px] font-bold text-[#000000]">
+            Drag here to delete
+          </h1>
         </div>
       </div>
     </>
