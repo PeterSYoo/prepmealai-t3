24f834c54f6eaa1949e78631ab858d9a6613e99a
Peter Yoo <trustintofu@gmail.com>
2023-03-21 21:59:57 -0700

    refactor(TypingText): Removes fading animations.

--- src/components/recipe/TypingText.components.tsx
+++ src/components/recipe/TypingText.components.tsx
@@ -1,21 +1,18 @@
 import { useEffect, useState } from "react";
 
-export const TypingText = ({ texts }: { texts: string[] }) => {
-  const [currentTextIndex, setCurrentTextIndex] = useState(0);
-  const [displayText, setDisplayText] = useState("");
-  const [isFadingOut, setIsFadingOut] = useState(false);
+export const TypingText = ({ texts }: { texts: [string] }) => {
+  const [currentTextIndex, setCurrentTextIndex] = useState<number>(0);
+  const [displayText, setDisplayText] = useState<string | undefined>("");
 
   useEffect(() => {
     let currentIndex = 0;
     const interval = setInterval(() => {
-      setDisplayText(texts[currentTextIndex].slice(0, currentIndex + 1));
+      setDisplayText(texts[currentTextIndex]?.slice(0, currentIndex + 1));
       currentIndex++;
-      if (currentIndex === texts[currentTextIndex].length) {
+      if (currentIndex === texts[currentTextIndex]?.length) {
         clearInterval(interval);
-        setIsFadingOut(true);
         setTimeout(() => {
           setCurrentTextIndex((currentTextIndex + 1) % texts.length);
-          setIsFadingOut(false);
         }, 2500);
       }
     }, 50);
@@ -24,11 +21,7 @@ export const TypingText = ({ texts }: { texts: string[] }) => {
 
   return (
     <section className="px-24">
-      <p
-        className={`bg-black bg-clip-text pb-32 text-2xl font-bold text-transparent ${
-          isFadingOut ? "animate-fade-out" : "animate-fade-in"
-        }`}
-      >
+      <p className="bg-black bg-clip-text pb-32 text-2xl font-bold">
         {displayText}
       </p>
     </section>
